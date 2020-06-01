23b6d356c1fb402911b9d8d060d5f0dbd368c456
biorover <mckensk0@gmail.com>
2020-06-01 18:42:35 -0400

    typo

--- HAP.py
+++ HAP.py
@@ -450,7 +450,7 @@ def emit_hmm_consensi(hmm_dir,out_dir,path_dict):
         out_file = open(out_dir + '/' + hmm.replace('.hmm','') + '.consensus.fa','w')
         emittext = subprocess.check_output(shlex.split(path_dict['hmmemit'] + ' -c ' + hmm_dir + '/' +
                                     hmm))
-        out_file.write(emittext.replace('-consenus'))
+        out_file.write(emittext.replace('-consenus','w'))
         out_file.close()
 
 

0f2604e13f14a1887c19341a6caed36d9584a380
Sean Mc Kenzie <smckenzi@login.curnagl>
2021-12-06 03:29:15 +0100

    added preliminary setup script

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='HAPpy-ABCENTH',
+    version='0.2.1',
+    packages=[
+        'HAPpy',
+        'ABCENTH'
+    ],
+    install_requires=[
+        'ete3',
+        'numpy',
+    ],
+)

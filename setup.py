d9366c42af9c64faf96568538b30efef4bad819d
Sean McKenzie <biorover@users.noreply.github.com>
2021-12-06 12:33:25 -0800

    updated setup to pass readme to pypi

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='HAPpy-ABCENTH',
@@ -19,5 +22,7 @@ setup(
             'HAPpy = HAPpy.__main__:main'
         ]
     },
-    include_package_data = True
+    include_package_data = True,
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )

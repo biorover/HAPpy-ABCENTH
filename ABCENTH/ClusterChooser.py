469ef8501f77080dc5eaa03d4eca2a5333e4434a
Sean McKenzie <biorover@users.noreply.github.com>
2022-01-03 15:24:40 -0800

    added ability to use base matches for cluster selection criterion

--- ABCENTH/ClusterChooser.py
+++ ABCENTH/ClusterChooser.py
@@ -4,7 +4,7 @@ import argparse,sys,os
 from intervaltree import IntervalTree
 import pandas as pd
 
-def chose_clusters(hittab,maxintron = 10000):
+def chose_clusters(hittab,maxintron = 10000, criterion = 'matches'):
     """
     reads through a hit table to find best-scoring non-overlapping clusters of hits
     """
@@ -15,27 +15,29 @@ def chose_clusters(hittab,maxintron = 10000):
     seqs = list(set(hittab['seqid']))
     seqdict = {seq:IntervalTree() for seq in seqs}
     loci = []
-    cols = ['cluster','seqid','start','end','score']
+    cols = ['cluster','seqid','start','end','score','matches']
     locrowis = {}
     for cluster in clusters:
         cdf = hittab.query('cluster == "' + cluster + '"')
         strand,ei,seqid,lastend = '.',0,'.',0
         for i,row in cdf.iterrows():
             start = min((row['sstart'],row['send']))
-            end = max((row['sstart'],row['send']))        
+            end = max((row['sstart'],row['send']))
+            idb += row['qlen'] * row['pid']    
             if row['seqid'] == seqid and row['strand'] == strand and end - lastend < maxintron and \
                     ((strand == "+" and row['ei'] > ei) or (strand == '-' and row['ei'] < ei)):
                 loci[-1][3] = end
                 loci[-1][4] += row['score']
+                loci[-1][5] += idb
                 locrowis[len(loci)].append(i)
             else:
-                loci.append([row['cluster'],row['seqid'],start,end,row['score']])
+                loci.append([row['cluster'],row['seqid'],start,end,row['score'],idb])
                 locrowis[len(loci)] = [i]
             strand,ei,seqid,lastend = row['strand'],row['ei'],row['seqid'],end
 
     locdf = pd.DataFrame(loci)
     locdf.columns = cols
-    locdf = locdf.sort_values('score',ascending = False)
+    locdf = locdf.sort_values(criterion,ascending = False)
     keepis = []
     for index,row in locdf.iterrows():
         start = min((row['start'],row['end']))
@@ -51,10 +53,11 @@ def main():
     parser = argparse.ArgumentParser("ClusterChooser: reads through a hit table to find best-scoring non-overlapping clusters of hits")
     parser.add_argument('--table', help = 'abcenth format hit table')
     parser.add_argument('--max_intron', help = 'maximum intron size (default = 10000', default = 10000, type = int)
+    parser.add_argument('--criterion', help = 'criterion for cluster selection (can be matches (default) or score)', defualt = 'matches')
     args = parser.parse_args()
 
     hittab = pd.read_csv(args.table,sep="\t",header=None)
-    hittab = chose_clusters(hittab,args.max_intron)
+    hittab = chose_clusters(hittab,args.max_intron,args.criterion)
     hittab.to_csv('/dev/stdout', sep = '\t', header = False, index = False)
 
 if __name__ == "__main__":
